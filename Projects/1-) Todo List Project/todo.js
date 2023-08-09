b33652a69bda88c42933386147fcd81a12d527fb
Oguzcan Ayan <oguzcanayan43@hotmail.com>
2023-08-09 18:01:40 +0300

    UpdatedJavaScriptCodesMustafaMuratCoskun

--- Projects/1-) Todo List Project/todo.js
+++ Projects/1-) Todo List Project/todo.js
@@ -11,15 +11,89 @@ eventListeners();
 
 function eventListeners() { //All event listeners
     form.addEventListener("submit", addTodo);
+    document.addEventListener("DOMContentLoaded", loadAllTodosToUI);
+    secondCardBody.addEventListener("click", deleteTodoFromUI);
+}
 
+function deleteTodoFromUI(e){
+    
+    if(e.target.className === "fa fa-remove"){
+        e.target.parentElement.parentElement.remove();
+        deleteTodoFromStorage(e.target.parentElement.parentElement.textContent);
+        showAlert("success", "Todo item has been deleted successfully");
+    }
+
+}
+function deleteTodoFromStorage(deletetodo){
+    let todos = getTodosFromStorage();
+    todos.forEach(function(todo, index){
+        if(todo === deletetodo){
+            todos.splice(index, 1); //We can delete an item from array
+        }
+        localStorage.setItem("todos", JSON.stringify(todos));
+
+    });
+}
+
+
+function loadAllTodosToUI(){
+       let todos =  getTodosFromStorage();
+
+       todos.forEach(function(todo) {
+       addTodoToUI(todo);
+       });
 }
 
 function addTodo(e) {
     const newTodo = todoInput.value.trim();
 
-    addTodoToUI(newTodo);
+    if (newTodo === "") {
+        showAlert("danger", "Please enter a todo value...");
+
+        /* <div class="alert alert-danger" role="alert">
+             This is a danger alert—check it out!
+           </div> */
+
+    } else {
+        addTodoToUI(newTodo);
+        addTodoToStorage(newTodo);
+        showAlert("success", "You entered a right value");
+    }
 
     e.preventDefault();
+}
+
+function getTodosFromStorage() { //Get todo values from the Storage
+    let todos;
+
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    return todos;
+}
+
+function addTodoToStorage(newTodo) {
+    let todos = getTodosFromStorage();
+    todos.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+}
+
+function showAlert(type, message) {
+    const alert = document.createElement("div");
+    alert.className = `alert alert-${type}`;
+    alert.textContent = message; /* alert.appendChild(document.createTextNode(message));  */
+
+    firstCardBody.appendChild(alert);
+
+    //setTimeOut
+
+    setTimeout(function () {
+        alert.remove();
+    }, 1500);
+
 }
 
 function addTodoToUI(newTodo) { //It inserts an entered string value to the UI
@@ -35,7 +109,7 @@ function addTodoToUI(newTodo) { //It inserts an entered string value to the UI
     </li> 
 
     */
-    
+
     //Creating List Item
     const listItem = document.createElement("li");
 
@@ -47,12 +121,12 @@ function addTodoToUI(newTodo) { //It inserts an entered string value to the UI
     link.innerHTML = "<i class='fa fa-remove'></i>";
 
     //Creating Text Node
-    listItem.appendChild(document.createTextNode(newTodo));
+    listItem.appendChild(document.createTextNode(newTodo)); /* listItem.textContent = newTodo; */
     listItem.appendChild(link);
 
     //Insert List Item to TodoList
     todoList.appendChild(listItem);
-    
+
     todoInput.value = "";
     // console.log(listItem);
 
