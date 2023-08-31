bddcdb06bf262d943ad1a84f2b2f822fca52918d
Oguzcan Ayan <oguzcanayan43@hotmail.com>
2023-08-31 17:11:12 +0300

    UpdatedJavaScriptCodesKodluyoruz

---
+++ Orta Seviyeler/23-) try-catch Structure/app.js
@@ -0,0 +1,28 @@
+
+let items;
+
+try {
+    items.forEach(item => {
+        console.log(item);
+    });   
+} catch (error) {
+    console.error("Error : ", error);
+    /* items = [1, 2, 3];
+    items.forEach(item => console.log(item)); */
+}
+
+console.log("Error Management is working successfully.");
+
+let info = "We are coding.";
+
+console.log(info);
+
+
+
+
+
+
+
+
+
+
