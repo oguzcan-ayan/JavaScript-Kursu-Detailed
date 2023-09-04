48728c7b56b82f381886e0e48934050e07213c0e
Oguzcan Ayan <oguzcanayan43@hotmail.com>
2023-09-04 12:32:32 +0300

    UpdatedJavaScriptCodesZamansizEgitimler

---
+++ Temeller/26-) Switch-Case Structure/app.js
@@ -0,0 +1,68 @@
+
+/* const name = "Oğuzcan";
+
+switch (name) {
+    case "Oğuz":
+        console.log("Result : " + name);
+        break;
+    case "Oğuzc":
+        console.log("Result : " + name);
+        break;
+    case "Oğuzcan":
+        console.log("Result : " + name);
+        break;
+    case "oğuzcan":
+        console.log("Result : " + name);
+        break;
+    case "oğuz":
+        console.log("Result : " + name);
+        break;
+    case "oğuzc":
+        console.log("Result : " + name);
+        break;
+
+    default:
+        console.log("Name is not found.");
+        break;
+} */
+
+const date = new Date().getDay();
+
+switch (date) {
+    case 0:
+        console.log("Sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+
+    default:
+        console.log("The day is not found.");
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
