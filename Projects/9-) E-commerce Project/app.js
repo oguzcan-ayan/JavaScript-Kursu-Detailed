6751198954e00e57ce591072ed8c09477e8d13c7
Oguzcan Ayan <oguzcanayan43@hotmail.com>
2023-09-13 17:09:08 +0300

    UpdatedEcommerceCodes

--- Projects/9-) E-commerce Project/app.js
+++ Projects/9-) E-commerce Project/app.js
@@ -1,4 +1,4 @@
-// import tt from './data.js'
+// // import tt from './data.js'
 
 const titleScroll = document.getElementById("title-scroll");
 
@@ -12,6 +12,66 @@ window.addEventListener('scroll', function(e){
         }
     }
 })
+
+// const slides = document.querySelectorAll(".slide");
+// const next = document.getElementById("next");
+// const previous = document.getElementById("prev");
+// const auto = true;
+// const intervalTime = 5000;
+// let slideInterval;
+// let currentIndex = 0;
+
+// const showSlides = (startIndex) =>{
+//         slides.forEach()
+
+
+// const nextSlide = () =>{
+
+//     const activeSlide = document.querySelectorAll(".active");
+//         if(activeSlide){
+//             activeSlide.classList.remove("active");
+//         }
+//         if(activeSlide.nextElementSibling){
+//             activeSlide.nextElementSibling.classList.add("active");
+//         }
+//         else{
+//             slides[0].classList.add("active");
+//         }
+// };
+
+// const previousSlide = () =>{
+
+//     const activeSlide = document.querySelectorAll(".active");
+//         if(activeSlide){
+//             activeSlide.classList.remove("active");
+//         }
+//         if(activeSlide.previousElementSibling){
+//             activeSlide.previousElementSibling.classList.add("active");
+//         }
+//         else{
+//             slides[slides.length - 1].classList.add("active");
+//         }
+// };
+
+// next.addEventListener("click", () =>{
+//     nextSlide();
+//     if(auto){
+//         clearInterval(slideInterval);
+//         slideInterval = setInterval(nextSlide, intervalTime);
+//     }
+// });
+
+// previous.addEventListener("click", () =>{
+//     previousSlide();
+//     if(auto){
+//         clearInterval(slideInterval);
+//         slideInterval = setInterval(previousSlide, intervalTime);
+//     }
+// });
+
+
+
+
 
 
 /* 
@@ -34,3 +94,28 @@ Object.entries(tt).map(el => console.log(el)) */
 /* let element = `<a>dsadsad</a>`
 
 divElementim.innerHTML += element; */
+
+let count = 0;
+
+document.getElementById('next').addEventListener("click", () => {
+    count++;
+    const translateValue = count * -110;
+    document.getElementById('start-slider-content').style = `transform: translateX(${translateValue}%);`
+    if(-translateValue >= 110){
+        count = 0;
+    }
+});
+document.getElementById('prev').addEventListener("click", () => {
+    if(count <= 0){
+        document.getElementById('start-slider-content').style = ``
+    }
+    else {
+        count--;
+        const translateValue = count * -50;
+        document.getElementById('start-slider-content').style = `transform: translateX(${translateValue}%);`
+    }
+
+});
+
+
+
